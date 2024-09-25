2f2e5899119d646e5b01c1790b705fc7ad373557
gioacostax <gioacostax@gmail.com>
2024-09-25 15:55:20 -0500

    feat: initial project

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="astro/client" />
+/// <reference types="vitest/globals" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_APP_NAME: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+type Fn = () => void;
+type Reject<T> = (reason: T) => void;
