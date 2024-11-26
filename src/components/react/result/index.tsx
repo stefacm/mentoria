42eeb8f183f325fc4750e50f866b4a0d5bc5ae66
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-11-26 08:52:24 -0500

    fix(audio): update audioRef when audioUrl changes

--- src/components/react/result/index.tsx
+++ src/components/react/result/index.tsx
@@ -1,12 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Play from '@static/assets/play.svg?react';
 
 import { GET_DICTIONARY } from '@/services/get_dictionary';
 import ServicesProvider, { useServiceQuery } from '@shared/react/services';
 
-// TODO: Subir los cambios al repositorio con mensajes claros.
-// TODO: Revisar y arreglar por qué no funciona el audio.
 // TODO: Separar el componente grande en partes más pequeñas para que sea más fácil de manejar.
 // TODO: Buscar otra forma de resolver lo que hicimos con || {} para que quede mejor.
 
@@ -15,7 +13,7 @@ const Result = () => {
   const { data } = useServiceQuery(GET_DICTIONARY, { axios: { url: word! }, enabled: !!word });
   const { meanings, phonetic, phonetics, sourceUrls } = data?.data[0] ?? {};
   const audioUrl = phonetics?.find((item) => item.audio !== '')?.audio;
-  const audioRef = useRef(audioUrl ? new Audio(audioUrl) : null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   const playAudio = () => {
     if (audioRef.current) {
@@ -24,6 +22,12 @@ const Result = () => {
       });
     }
   };
+
+  useEffect(() => {
+    if (audioUrl) {
+      audioRef.current = new Audio(audioUrl);
+    }
+  }, [audioUrl]);
 
   return (
     <div className="flex h-full w-full max-w-3xl flex-col gap-8 md:gap-10">
