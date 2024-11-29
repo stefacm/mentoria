dca292a8143288f21a16045cc8ad1f3d749cdd03
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-11-29 17:34:55 -0500

    refactor(button): separate play button logic into appropriate component

--- src/components/react/result/header.tsx
+++ src/components/react/result/header.tsx
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { type FC, useRef } from 'react';
 
 import Play from '@static/assets/play.svg?react';
 
 interface HeaderProps {
   audioUrl?: string;
   phonetic?: string;
-  playAudio: () => void;
   word: string | null;
 }
 
-const Header: React.FC<HeaderProps> = ({ audioUrl, phonetic, playAudio, word }) => (
-  <section className="flex w-full justify-between space-x-4">
-    <div className="flex flex-col space-y-3">
-      <h1 className="text-2xl font-bold md:text-6xl">{word}</h1>
-      <p className="text-lg text-purple-600 md:text-2xl">{phonetic}</p>
-    </div>
-    {audioUrl && (
-      <button
-        className="flex h-12 w-12 items-center justify-center rounded-full bg-transparent hover:bg-black hover:bg-opacity-5 focus:outline-purple-500 md:h-20 md:w-20"
-        onClick={playAudio}
-      >
-        <Play className="h-12 w-12 md:h-20 md:w-20" height={20} width={20} />
-      </button>
-    )}
-  </section>
-);
+const Header: FC<HeaderProps> = ({ audioUrl, phonetic, word }) => {
+  const audioRef = useRef<HTMLAudioElement | null>(audioUrl ? new Audio(audioUrl) : null);
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      void audioRef.current.play();
+    }
+  };
+
+  return (
+    <section className="flex w-full justify-between space-x-4">
+      <div className="flex flex-col space-y-3">
+        <h1 className="text-2xl font-bold md:text-6xl">{word}</h1>
+        <p className="text-lg text-purple-600 md:text-2xl">{phonetic}</p>
+      </div>
+      {audioUrl && (
+        <button
+          className="flex h-12 w-12 items-center justify-center rounded-full bg-transparent hover:bg-black hover:bg-opacity-5 focus:outline-purple-500 md:h-20 md:w-20"
+          onClick={playAudio}
+        >
+          <Play className="h-12 w-12 md:h-20 md:w-20" height={20} width={20} />
+        </button>
+      )}
+    </section>
+  );
+};
 
 export default Header;
--- src/components/react/result/index.tsx
+++ src/components/react/result/index.tsx
@@ -1,42 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 import { GET_DICTIONARY } from '@/services/get_dictionary';
 import ServicesProvider, { useServiceQuery } from '@shared/react/services';
+import Shimmer from '@shared/react/shimmer';
 
 import Header from './header';
 import MeaningSection from './meaningSection';
 import SourceSection from './sourceSection';
 
-// TODO: Buscar otra forma de resolver lo que hicimos con ?? {} para que quede mejor.
-
 const Result = () => {
   const word = new URLSearchParams(location.search).get('word');
-  const { data } = useServiceQuery(GET_DICTIONARY, { axios: { url: word! }, enabled: !!word });
-  const { meanings, phonetic, phonetics, sourceUrls } = data?.data[0] ?? {};
-  const audioUrl = phonetics?.find((item) => item.audio !== '')?.audio;
-  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const { data, isError, isFetching, isSuccess } = useServiceQuery(GET_DICTIONARY, {
+    axios: { url: word! },
+    enabled: !!word,
+  });
 
-  const playAudio = () => {
-    if (audioRef.current) {
-      audioRef.current.play().catch((error) => {
-        console.error('Error al reproducir el audio:', error);
-      });
-    }
-  };
+  if (isFetching)
+    return (
+      <div className="flex h-full w-full max-w-3xl flex-col gap-8 md:gap-10">
+      </div>
+    );
 
-  useEffect(() => {
-    if (audioUrl) {
-      audioRef.current = new Audio(audioUrl);
-    }
-  }, [audioUrl]);
+  if (isError) return null; // TODO: Mensaje error
 
-  return (
-    <div className="flex h-full w-full max-w-3xl flex-col gap-8 md:gap-10">
-      <Header audioUrl={audioUrl} phonetic={phonetic} playAudio={playAudio} word={word} />
-      {meanings?.map((mean) => <MeaningSection key={mean.partOfSpeech} meaning={mean} />)}
-      {!!sourceUrls?.length && <SourceSection sourceUrls={sourceUrls} />}
-    </div>
-  );
+  if (!data?.data.length && isSuccess)
+    return null; // TODO: Emoji de no resultados
+  else if (data?.data.length) {
+    const { meanings, phonetic, phonetics, sourceUrls } = data.data.at(0)!;
+    const audioUrl = phonetics.find((item) => item.audio !== '')?.audio;
+
+    return (
+      <div className="flex h-full w-full max-w-3xl flex-col gap-8 md:gap-10">
+        <Header audioUrl={audioUrl} phonetic={phonetic} word={word} />
+        {meanings.map((mean) => (
+          <MeaningSection key={mean.partOfSpeech} meaning={mean} />
+        ))}
+        {!!sourceUrls.length && <SourceSection sourceUrls={sourceUrls} />}
+      </div>
+    );
+  }
 };
 
 export default Result;
