e7d64440153bff00b50ec1695b5a6bf8107b5ef4
Stefany Cardenas Martinez <stefany.cardenas@pragma.com.co>
2024-12-15 06:07:38 -0500

    test(utils): add location mocks

---
+++ shared/vitest/dom.utils.ts
@@ -0,0 +1,28 @@
+/**
+ * This file can be used to define DOM test utils
+ */
+
+import { screen } from '@testing-library/dom';
+
+//////////////////////////////////////////////////////////////
+export const locationAssignMock = (href = 'https://localhost') => {
+  const assign = vi.fn();
+  Object.defineProperty(window, 'location', {
+    configurable: true,
+    value: new URL(href),
+  });
+  window.location.assign = assign;
+  return assign;
+};
+
+//////////////////////////////////////////////////////////////
+
+export const locationReplaceMock = (href = 'https://localhost') => {
+  const replace = vi.fn();
+  Object.defineProperty(window, 'location', {
+    configurable: true,
+    value: new URL(href),
+  });
+  window.location.replace = replace;
+  return replace;
+};
